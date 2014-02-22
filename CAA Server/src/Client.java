f88151287527f606f0d807666c2948e647b0b910
Brennan L <bl27592@gmail.com>
2014-02-22 14:17:54 -0700

    Server starting code added

---
+++ CAA Server/src/Client.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class Client {
+
+	public Socket socket;
+	public PrintWriter out;
+	public BufferedReader in;
+	
+}
